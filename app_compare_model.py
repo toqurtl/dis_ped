1a818406710ff274d590d8a3165735141bc1af60
toqurtl <yoon92411@naver.com>
2022-03-28 11:55:19 +0900

    change setting

--- app_compare_model.py
+++ app_compare_model.py
@@ -1,26 +1,31 @@
 import os
 from dis_ped.config.filefinder import FileFinder
-import sys
+import json
+import csv
 
-setting_folder = sys.argv[1]
+folder_path = "D:\\OneDrive\\연구\\pandemic\\experiment\\0326"
+file_name = "compare_idx.json"
+result_file_name = "compare_model.csv"
+first_row = ["model_id", "type", "ade_avg", "dtw_avg", "social_avg", "ctn"]
+first_row += ["ade_avg_low", "dtw_avg_low", "ctn_low"]
+first_row += ["ade_avg_high", "dtw_avg_high", "ctn_high"]
 
-setting_path_list = []
-for path, dirs, files in os.walk(setting_folder):
-    for file in files:
-        if file.split(".")[-1] == "json":
-            setting_path_list.append(os.path.join(path, file))
+result_file_path = os.path.join(folder_path, result_file_name)
+csv_f = open(result_file_path, "w", newline='')
+wr = csv.writer(csv_f)
+wr.writerow(first_row)
 
 
-for set_idx, setting_path in enumerate(setting_path_list):
-    # finder = FileFinder(setting_path)
-    # print("start")
-    # vid_folder_list = finder.get_vid_folder_list()
-    # total = len(vid_folder_list)    
-    # for progress_idx, folder_path in enumerate(vid_folder_list):
-    #     idx = folder_path.split("\\")[-1]
-    #     os.system("python app_simulate.py "+idx+" "+setting_path)
-    #     os.system("python app_validate.py "+idx+" "+setting_path)
-    #     print(str(progress_idx)+"/"+str(total), str(set_idx)+"/"+str(len(setting_path_list)))
-
-    # os.system("python app_compare.py " + setting_path)
-    os.system("python app_aggregate.py " + setting_path)
+for folder_name in os.listdir(folder_path):
+    if folder_name.split(".")[-1] != "csv":
+        model_path = os.path.join(folder_path, folder_name)
+        print(model_path)
+        if file_name in os.listdir(model_path):
+            file_path = os.path.join(model_path, file_name)
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            row = []
+            for x in data["state"].values():
+                row.append(x)
+            wr.writerow(row)
+csv_f.close()
--- app_experiment.py
+++ app_experiment.py
@@ -12,15 +12,15 @@ for path, dirs, files in os.walk(setting_folder):
 
 
 for set_idx, setting_path in enumerate(setting_path_list):
-    # finder = FileFinder(setting_path)
-    # print("start")
-    # vid_folder_list = finder.get_vid_folder_list()
-    # total = len(vid_folder_list)    
-    # for progress_idx, folder_path in enumerate(vid_folder_list):
-    #     idx = folder_path.split("\\")[-1]
-    #     os.system("python app_simulate.py "+idx+" "+setting_path)
-    #     os.system("python app_validate.py "+idx+" "+setting_path)
-    #     print(str(progress_idx)+"/"+str(total), str(set_idx)+"/"+str(len(setting_path_list)))
+    finder = FileFinder(setting_path)
+    print("start")
+    vid_folder_list = finder.get_vid_folder_list()
+    total = len(vid_folder_list)    
+    for progress_idx, folder_path in enumerate(vid_folder_list):
+        idx = folder_path.split("\\")[-1]
+        os.system("python app_simulate.py "+idx+" "+setting_path)
+        os.system("python app_validate.py "+idx+" "+setting_path)
+        print(str(progress_idx)+"/"+str(total), str(set_idx)+"/"+str(len(setting_path_list)))
 
-    # os.system("python app_compare.py " + setting_path)
+    os.system("python app_compare.py " + setting_path)
     os.system("python app_aggregate.py " + setting_path)
