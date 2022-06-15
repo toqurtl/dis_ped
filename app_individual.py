de44bb872e3230cb8e3c34ef835197a7abeb8803
toqurtl <yoon92411@snu.ac.kr>
2022-06-15 16:40:58 +0900

    app_individual

--- app_individual.py
+++ app_individual.py
@@ -11,18 +11,23 @@ import sys
 
 target_force_type = sys.argv[1]
 folder_path = "C:\\Users\\toqurtl\\snucem\\exp\\result\\0326"
-result_file_name = "person_"+target_force_type+"_0607.json"
-result_file_name_csv = "person_"+target_force_type+"_0607.csv"
+result_file_name = "person_"+target_force_type+"_0607_dtw.json"
+result_file_name_csv = "person_"+target_force_type+"_0607_dtw.csv"
 
 result_file_path = os.path.join(folder_path, result_file_name)
 result_file_path_csv = os.path.join(folder_path, result_file_name_csv)
 
+# force_1
+# model_id_list = [1,7,6,14,13,11,10,12,15,16,17,18, 19, 20, 21, 22, 23,24,25,26,27,28,29,30,31,32,33,34]
+# model_id_list = [12,16,17,15,6,19,20,21,22,1,23,24,25,26,27,28,29,30,31,32,33]
+
 # force_2
 # model_id_list =[1,7,6,13,10,9,12,8,11,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34, 35]
-#force_1
-model_id_list = [1,7,6,14,13,11,10,12,15,16,17,18, 19, 20, 21, 22, 23,24,25,26,27,28,29,30,31,32,33,34]
-# basic-sfm-1
-# model_id_list = [8]
+model_id_list = [16,21,11,12,10,6,18,20,23,24,1,25,26,27,28,29,30,31,32,33,34,35]
+
+
+# # basic-sfm-1
+# model_id_list = [1]
 
 result_dict ={}
 for folder_name in os.listdir(folder_path):
@@ -46,7 +51,7 @@ model_id_list.pop(0)
 for vid_name, vid_dict in result_dict.items():
     value_list=[vid_name]        
     for i in model_id_list:
-        model_name = "social_sfm_1_"+str(i)
+        model_name = "social_sfm_2_"+str(i)
         if model_name in vid_dict.keys():
             value_list.append(vid_dict[model_name])
         else:
